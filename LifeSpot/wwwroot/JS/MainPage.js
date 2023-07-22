31b659eeaf6e5e9478a8af988392785cba9991c5
PavlenkoIGOR <illusiopard@gmail.com>
2023-07-22 23:43:56 +0300

    поиск

--- LifeSpot/wwwroot/JS/MainPage.js
+++ LifeSpot/wwwroot/JS/MainPage.js
@@ -1,9 +1,17 @@
-﻿
-
-function SeekAndShow() {
+﻿function SeekAndShow() {
+    // Получим все контейнеры с видео
     let elements = document.getElementsByClassName("video-container");
-    // Пробежимся в цикле и выведем все в консоль
+
+    // Пробежимся в цикле по контейнерам
     for (let i = 0; i <= elements.length; i++) {
-        console.log(elements[i])
+
+        // Получим всё что внутри контейнера
+        let childElements = elements[i];
+        // Получим элемент, содержащий описание видео
+        // Он у нас единственный с тегом h3, снова воспользуемся поиском по тегу,
+        let videoDescription = childElements.getElementsByTagName('h3')[0];
+        // Выведем его текст на консоль
+        console.log(videoDescription.innerText); 
     }
-}
+};
+
