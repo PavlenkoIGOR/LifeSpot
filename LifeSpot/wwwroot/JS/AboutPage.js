bd74eee373cf16d4decc81c2039a8d7043a1d602
PavlenkoIGOR <illusiopard@gmail.com>
2023-08-01 20:50:55 +0300

    добавлена функция для подсчета лайков или дизлайков

--- LifeSpot/wwwroot/JS/AboutPage.js
+++ LifeSpot/wwwroot/JS/AboutPage.js
@@ -1,68 +1,81 @@
 ﻿function SetComment() {
-    let UserData = {};//создается объект для хранения данных пользователя (имя и комментарий)
+
+    let UserData = {};//создается объект для хранения данных пользователя (имя и комментарий) (да, ради прикола)
     UserData.userName = prompt("Введите Ваше имя: ");//запрос имени и запись в объект
     UserData.userComment = prompt("Введите свой комментарий: "); //запрос и запись комментария в объект
     UserData.commentDate = new Date().toLocaleString();
+
+    let likes = 0;
+    let dislikes = 0;
 
     if ((UserData.userName != '' || UserData.userComment != '') & (UserData.userName != null || UserData.userComment != null)) {
 
         let div = document.getElementById('mainCommentsField'); //выбор элемента по классу (родительский) - поле на котором все комментарии
 
-        let commentDivCommentDivLikes = document.createElement('div'); //создание дочернего div в котором будут поле с [комментарии, данные] и поле с [кнопки и счетчик]
-        commentDivCommentDivLikes.className = 'commentDivCommentDivLikes';
+        let divCommentDataLikes = document.createElement('div'); //создание дочернего div в котором будут поле с [комментарии, данные] и поле с [кнопки и счетчики]
+        divCommentDataLikes.className = 'commentDivCommentDivLikes';
 
-        let commentDivTextAndData = document.createElement('div'); //создание дочернего div в котором будут комментарии и данные
-        let commentDivUser = document.createElement("div"); //создание в дочернем div div'а с данными юзверя
-        let commentDivText = document.createElement("div");//создание в дочернем div div'а с текстом комментария
+        let divUserData = document.createElement("div"); //создание в дочернем div div'а с данными юзверя
+        divUserData.className = "divUserData"; //присвоение класса div'у с данными юзверя
+        divUserData.textContent = "Пользователь: \n" + UserData.userName + "\n" + UserData.commentDate;  //вывод данных пользователя
 
-        commentDivTextAndData.className = "commentDivTextAndData";
-        commentDivText.className = "commentDivText"; //присвоение класса div'у с текстом комментария
-        commentDivUser.className = "commentDivUser"; //присвоение класса div'у с данными юзверя
+        let divCommentText = document.createElement("div");//создание в дочернем div div'а с текстом комментария
+        divCommentText.className = "divCommentText"; //присвоение класса div'у с текстом комментария
+        divCommentText.textContent = UserData.userComment;
 
-        commentDivText.textContent = UserData.userComment;
-        commentDivUser.textContent = "Пользователь: \n" + UserData.userName + "\n" + UserData.commentDate;  //вывод данных пользователя
 
-        //создание div для расположения в нем 4-х полей: поле счётчика лайков||кнопка 'LIKE'||кнопка 'DIASLIKE'||поле счетчика дизлайков
-        let divForLikeDislike = document.createElement('div');
-        divForLikeDislike.className = 'divForLikeDislike';
+        
+        let divLikesDislikes = document.createElement('div'); //создание в дочернем div div'а с [div с кнопкой Like||div со счетчиком лайков||div с кнопкой Like||div со счетчиком дизлайков]
+        divLikesDislikes.className = 'divLikesDislikes';
 
-        //создание div счетчика лайков
-        let likesDiv = document.createElement('div');
-        likesDiv.className = 'likesDiv';
-        likesDiv.id = 'likesDiv';
-        //создание кнопки 'Like'
-        let likeButton = document.createElement('button');
-        likeButton.className = 'likeButton';
-        likeButton.textContent = 'Нравится';
-        likeButton.id = 'likeButton';
+        let divLikesCount = document.createElement('div'); //создание в дочернем div div'а с счётчиком лайков
+        divLikesCount.className = 'divLikesCount';
+        
 
-        //создание кнопки 'DisLike'
-        let disLikeButton = document.createElement('button');
-        disLikeButton.className = 'disLikeButton';
-        disLikeButton.textContent = 'Не нравится';
-        //создание div счетчика дизлайков
-        let disLikesDiv = document.createElement('div');
-        disLikesDiv.className = 'disLikesDiv';
+        let divDislikesCount = document.createElement('div'); //создание в дочернем div div'а с счётчиком дизлайков
+        divDislikesCount.className = 'divDislikesCount';
+        
 
-        commentDivTextAndData.appendChild(commentDivUser);
-        commentDivTextAndData.appendChild(commentDivText);
+        let divLikesButton = document.createElement('div'); //создание в дочернем div div'а с кнопкой Like
+        
+        let divDislikesButton = document.createElement('div'); //создание в дочернем div div'а с кнопкой Dislike
+        
 
-        divForLikeDislike.appendChild(likesDiv);
-        divForLikeDislike.appendChild(likeButton);
-        divForLikeDislike.appendChild(disLikeButton);
-        divForLikeDislike.appendChild(disLikesDiv);
+        let likesButton = document.createElement('button'); //создание в div'е [divLikesButton] кнопки Like
+        likesButton.textContent = 'Нравится';
+        let dislikesButton = document.createElement('button'); //создание в div'е [divDislikesButton] кнопки dislike
+        dislikesButton.textContent = 'Не нравится';
 
-        commentDivCommentDivLikes.appendChild(commentDivTextAndData);
-        commentDivCommentDivLikes.appendChild(divForLikeDislike);
 
-        div.appendChild(commentDivCommentDivLikes);
+        divLikesCount.textContent = CountLikesOrDislikes(divLikesCount, likesButton, likes);
+        divDislikesCount.textContent = CountLikesOrDislikes(divDislikesCount, dislikesButton, dislikes);
 
-        let a = document.getElementById('likeButton');
-        a.onclick() = function () {
-            document.getElementById('likesDiv').textContent = 'лайк';
-        }
+
+        divLikesButton.appendChild(likesButton);
+        divDislikesButton.appendChild(dislikesButton);
+
+        divLikesDislikes.appendChild(divLikesButton);
+        divLikesDislikes.appendChild(divLikesCount);
+        divLikesDislikes.appendChild(divDislikesButton);
+        divLikesDislikes.appendChild(divDislikesCount);
+
+        divCommentDataLikes.appendChild(divUserData);
+        divCommentDataLikes.appendChild(divCommentText);
+        divCommentDataLikes.appendChild(divLikesDislikes);
+
+        
+        div.appendChild(divCommentDataLikes);
+
     }
     else {
         return;
+    }
+}
+
+//функция для подсчета лайков или дизлайков
+function CountLikesOrDislikes(obj, button, counter) {
+    button.onclick = () => {
+        counter++;
+        return obj.textContent = counter;
     }
 }
