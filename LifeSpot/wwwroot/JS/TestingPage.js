0153a37f72522aa720d6537f0224db4a5799d395
PavlenkoIGOR <illusiopard@gmail.com>
2023-08-03 19:21:36 +0300

    добавлен метод запоминания двух последних запросов на TestingPage

--- LifeSpot/wwwroot/JS/TestingPage.js
+++ LifeSpot/wwwroot/JS/TestingPage.js
@@ -1,7 +1,20 @@
-﻿
+﻿/*
 var userName = prompt('Ваше имя?');
-alert('Значит Вы - ' + userName + " и длина имени " + userName.length + ' символов.');
+alert('Значит, Вы - ' + userName + " и длина имени " + userName.length + ' символов.');
 function outputData()
 {
     alert(document.getElementById('inputBordered').value);
+}
+*/
+
+//функция (или объект в JS) для поиска видео
+const SomethingInput = function () {
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+    // Покажем окно с прошлым и новым значением
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    // Сохраним новое значение в контекст
+    this.lastInput = currentInput;
 }
